7a0dc31c22a94a25dbc0f5c3cd4c966e7de3858c
ShihFengLin <noahleft@gmail.com>
2020-01-04 00:18:57 +0800

    update float precision

--- docs/regions/postprocess.py
+++ docs/regions/postprocess.py
@@ -11,7 +11,6 @@ import re
 for f in filelist:
   with open(f,'r') as infile:
     strlines = infile.readlines()
-  print(f, end="\t")
   paths = [line for line in strlines if 'new google.maps.LatLng' in line]
   paths = paths[min([idx for idx in range(len(paths)) if paths[idx][0]=='[']):]
   pattern = re.compile('.*\((\d+\.\d+), (\d+\.\d+)\)')
@@ -19,13 +18,12 @@ for f in filelist:
   mean_lat = (max(lat)+min(lat))/2
   lng = list(map(lambda x: float(pattern.match(x).group(2)), paths))
   mean_lng = (max(lng)+min(lng))/2
-  print(mean_lat,mean_lng)
   with open(f,'w') as outfile:
     for strline in strlines:
       if "lat:" in strline:
-        outfile.write("    lat: "+str(mean_lat)+",\n")
+        outfile.write("    lat: {:.8f}.\n".format(mean_lat))
       elif "lng:" in strline:
-        outfile.write("    lng: "+str(mean_lng)+"\n")
+        outfile.write("    lng: {:.8f}.\n".format(mean_lng))
       else:
         outfile.write(strline)
 
