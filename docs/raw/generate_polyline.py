a8f91cf565173eaedb1d1c54b9f081e57d8f4743
ShihFengLin <noahleft@gmail.com>
2020-06-29 01:44:20 +0800

    fix broken region r20, r22, r6

--- docs/raw/generate_polyline.py
+++ docs/raw/generate_polyline.py
@@ -12,6 +12,20 @@ var polyline = new google.maps.Polyline({path:path, strokeColor: "polycolor", st
 polyline.setMap(map);
 """
 
+maskstring = """
+var polygonMask = new google.maps.Polygon({
+map:map,
+strokeColor: '#000000',
+strokeOpacity: 0.5,
+strokeWeight: 2,
+fillColor: '#CACACA',
+fillOpacity: 0.7,
+paths: [
+
+	RegionMask
+]});
+"""
+
 with open('polytemplate.html','r') as infile:
     template = infile.read()
 
@@ -25,6 +39,9 @@ def extractLatLng(filepath):
 def generatePoly(latlng, color="#FF0000"):
     return polystring.replace("polypath",','.join(['new google.maps.LatLng('+p[0]+','+p[1]+')' for p in latlng])).replace("polycolor",color)
 
+def generateMask(latlng):
+    return maskstring.replace('RegionMask',','.join(['new google.maps.LatLng('+p[0]+','+p[1]+')' for p in latlng]))
+
 def calCenter(latlng):
     lats = [float(s[0]) for s in latlng]
     lngs = [float(s[1]) for s in latlng]
@@ -34,20 +51,28 @@ def calCenter(latlng):
 
 colorMap = {"L01":"red","L02":"blue","L03":"black","L04":"green","L05":"brown","L06":"darkgray","L07":"aqua","L08":"darkblue","L09":"darkred","L10":"darkgreen"}
 
-
 polys = ""
+masks = ""
 for d in dirlist:
     print('process',d)
     filelist = [s for s in listdir(d) if match(r"R\d+L\d*",s)]
     latlng = []
     for f in filelist:
         if '_' in f or '-' in f:
-            pass
+            tmp = extractLatLng('/'.join([d,f]))[::-1]
+            latlng+=tmp
         else:
             color = colorMap[f[f.index('L'):]]
             tmp = extractLatLng('/'.join([d,f]))
             polys+=generatePoly(tmp, color)
-content = template.replace('RegionPoly',polys)
+            latlng+=tmp
+    if d!='region99':
+        masks+=generateMask(latlng)
 
+content = template.replace('RegionPoly',polys)
 with open('region_poly.html','w') as outfile:
+    outfile.write(content)
+
+content = template.replace('RegionPoly',masks)
+with open('region_overview.html','w') as outfile:
     outfile.write(content)
